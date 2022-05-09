0ff4f9e5b5638f1991d5241d1ac90526cd6a1e33
Marc Laurent Zuber <marc.zuber@gmail.com>
2022-05-09 17:13:46 +0200

    Fire Spell

--- Assets/Lorro_Assets/Spells/FireSpell/FireSpell.cs
+++ Assets/Lorro_Assets/Spells/FireSpell/FireSpell.cs
@@ -7,23 +7,27 @@ public class FireSpell : Spell
     public float speed = 5f;
     public float TTL = 10;
 
+    public List<AudioClip> castSounds;
+    public AudioClip flightSound;
+    public List<AudioClip> impactSounds;
+
     private Vector3 direction;
     private SphereCollider sc;
+    private AudioSource ac;
 
     private Vector3 origin;
 
     // Start is called before the first frame update
     void Start()
     {
-        sc = GetComponent<SphereCollider>();
-        //sc.isTrigger = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += direction * Time.deltaTime * speed;
-        transform.position += new Vector3((Random.value - 0.5f) * Time.deltaTime * (speed/5), (Random.value - 0.5f) * Time.deltaTime * (speed / 5), (Random.value - 0.5f) * Time.deltaTime * (speed / 5));
+        //transform.position += new Vector3((Random.value - 0.5f) * Time.deltaTime * (speed/5), (Random.value - 0.5f) * Time.deltaTime * (speed / 5), (Random.value - 0.5f) * Time.deltaTime * (speed / 5));
 
         TTL -= Time.deltaTime;
 
@@ -35,6 +39,9 @@ public class FireSpell : Spell
 
     public override void Execute()
     {
+        ac = GetComponent<AudioSource>();
+        sc = GetComponent<SphereCollider>();
+
         SpellManager spellManager = GameObject.Find("SpellManager").GetComponent<SpellManager>();
 
         GameObject castPoint = spellManager.GetCastPoint();
@@ -43,5 +50,8 @@ public class FireSpell : Spell
         transform.position = castPoint.transform.position;
         direction = castPoint.transform.forward;
         origin = transform.position;
+
+        ac.PlayOneShot(castSounds[Random.Range(0, castSounds.Count)]);
+        ac.PlayOneShot(flightSound);
     }
 }
